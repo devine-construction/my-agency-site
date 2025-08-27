06c013fe48e26c49c62efc1ceb9e19a11f1dd2f7
devine-construction <63998317+devine-construction@users.noreply.github.com>
2025-08-27 11:03:07 +1000

    form fixes

--- src/GetBuilding.js
+++ src/GetBuilding.js
@@ -29,37 +29,20 @@ export default function GetBuilding() {
 
       {/* -------- Netlify form -------- */}
       <form
-        name="build-contact"
-        method="POST"
-        data-netlify="true"
-        className="build-form"
-      >
-        <input type="hidden" name="form-name" value="build-contact" />
+  name="build-contact"
+  method="POST"
+  data-netlify="true"
+  netlify-honeypot="bot-field"
+  action="/thanks"                // or leave off to stay on /build
+  className="build-form"
+>
+  <input type="hidden" name="form-name" value="build-contact" />
+  <p hidden>
+    <label>Don’t fill this out: <input name="bot-field" /></label>
+  </p>
+  {/* …your fields… */}
+</form>
 
-        <label>
-          Your name
-          <input type="text" name="name" required />
-        </label>
-
-        <label>
-          Company / project
-          <input type="text" name="company" />
-        </label>
-
-        <label>
-          Email
-          <input type="email" name="email" required />
-        </label>
-
-        <label>
-          What are you looking to achieve?
-          <textarea name="message" rows="5" required />
-        </label>
-
-        <button type="submit" className="cta primary">
-          Get Building
-        </button>
-      </form>
 
       {/* -------- alternate contact -------- */}
       <p className="alt-contact">
